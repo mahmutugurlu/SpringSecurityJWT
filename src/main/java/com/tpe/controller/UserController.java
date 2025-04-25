05e2dd791964b121ae9ea2cba6160fb32dfe71e1
Mahmut Ugurlu <mahmutugurlu0718@gmail.com>
2025-04-25 22:21:51 +0200

    firstday2

--- src/main/java/com/tpe/controller/UserController.java
+++ src/main/java/com/tpe/controller/UserController.java
@@ -1,22 +1,28 @@
 package com.tpe.controller;
 
+import com.tpe.dto.LoginUser;
 import com.tpe.dto.UserDTO;
+import com.tpe.security.JwtUtils;
 import com.tpe.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtUtils jwtUtils;
 
     //userı kaydetme:register
     //request:http://localhost:8080/register + POST + Body
@@ -26,17 +32,61 @@ public class UserController {
       "username":"alican",
       "password":"12345678"
     }
-     */
-
+    */
     //response:save + 201 + mesaj
     @RequestMapping("/register")
     @PostMapping
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserDTO userDTO){
 
         userService.saveUser(userDTO);
 
         return new ResponseEntity<>("User is registered successfully...", HttpStatus.CREATED);
+    }
+
+    //user login
+    //request:http://localhost:8080/login + JSON body(username+password) + POST
+    //response:authenticate(kimliklendirme)-->authentication
+    //        :token üretip vereceğiz
+    @RequestMapping("/login")
+    @PostMapping
+    public ResponseEntity<String> login(@Valid @RequestBody LoginUser loginUser){
+
+        //username ve password ile doğrulama(kimliklendirme)
+        Authentication authentication =authenticationManager.
+                authenticate(new UsernamePasswordAuthenticationToken(loginUser.getUserName(),
+                        loginUser.getPassword()));
+        // authenticate : parametre olarak UsernamePasswordAuthenticationToken objesi alır
+        //bu objenin isAuthenticated özelliği başlangıçta false'dur
+        //eğer doğrulama başarılı ise metod geriye authentication return eder ve
+        //isAuthenticated true olarak set edilir.
+
+        String token= jwtUtils.generateToken(authentication);
+
+        return new ResponseEntity<>(token,HttpStatus.CREATED);
 
     }
+
+
+    @RequestMapping("/goodbye")
+    @GetMapping
+    @PreAuthorize("hasAnyRole('STUDENT','INSTRUCTOR','ADMIN')")
+    public ResponseEntity<String> goodbye(){
+        return ResponseEntity.ok("Goodbye Security:)");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/tpe/security/config/WebSecurityConfig.java
+++ src/main/java/com/tpe/security/config/WebSecurityConfig.java
@@ -2,10 +2,20 @@ package com.tpe.security.config;
 
 import com.tpe.security.AuthTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +27,47 @@ public class WebSecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //REST apide session yok
+                .and()
+                .authorizeRequests()
+                .antMatchers("/register","/login","/home").permitAll()
+                .antMatchers("/students").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
 
+        http.addFilterBefore(authTokenFilter,UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+
+    }
+
+    //authentication provider beani üretelim
+    @Bean
+    public DaoAuthenticationProvider provider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    //authentication provider password encoder beani üretelim
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();//10
+    }
+
+    //authentication manager beani üretelim
+    @Bean
+    public AuthenticationManager authenticationManager
+    (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
 
 
 
